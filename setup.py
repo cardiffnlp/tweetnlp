905dba08c7026db8f1c351c693b8b6c370eca507
asahi417 <asahi1992ushio@gmail.com>
2022-06-30 13:55:04 +0100

    fix the error in setup file on windows https://github.com/cardiffnlp/tweetnlp/issues/1\#issue-1289996730

--- setup.py
+++ setup.py
@@ -1,7 +1,8 @@
 from setuptools import setup, find_packages
 
-with open('README.md', 'r') as f:
+with open('README.md', 'r', encoding="utf-8") as f:
     readme = f.read()
+
 version = '0.0.8'
 setup(
     name='tweetnlp',
