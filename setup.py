641333732d9a43f3ba885e39ec69648fb248f57c
asahi417 <asahi1992ushio@gmail.com>
2022-07-04 14:26:20 +0100

    new version

--- setup.py
+++ setup.py
@@ -3,7 +3,7 @@ from setuptools import setup, find_packages
 with open('README.md', 'r', encoding="utf-8") as f:
     readme = f.read()
 
-version = '0.0.8'
+version = '0.0.9'
 setup(
     name='tweetnlp',
     packages=find_packages(exclude=["asset", "tests"]),
