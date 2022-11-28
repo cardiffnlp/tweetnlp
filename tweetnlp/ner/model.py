62f589a962805eaa71c09f562b785b75eba30722
asahi417 <asahi1992ushio@gmail.com>
2022-11-28 10:51:01 +0000

    refactor ner model

--- tweetnlp/ner/model.py
+++ tweetnlp/ner/model.py
@@ -1,8 +1,9 @@
-# TODO: Fix prediction
 import logging
 from typing import List
-
+from statistics import mean
 import torch
+# from tweetnlp.ner.allennlp_crf import ConditionalRandomField, allowed_transitions
+# from tweetnlp.util import load_model, get_preprocessor
 
 from .allennlp_crf import ConditionalRandomField, allowed_transitions
 from ..util import load_model, get_preprocessor
@@ -11,65 +12,13 @@ from ..util import load_model, get_preprocessor
 DEFAULT_MODEL = "tner/roberta-large-tweetner7-all"
 
 
-def decode_ner_tags(tag_sequence, input_sequence, probability_sequence=None):
-
-    def update_collection(_tmp_entity, _tmp_entity_type, _tmp_prob, _tmp_pos, _out):
-        if len(_tmp_entity) != 0 and _tmp_entity_type is not None:
-            if _tmp_prob is None:
-                _out.append({'type': _tmp_entity_type, 'entity': _tmp_entity, 'position': _tmp_pos})
-            else:
-                _out.append({'type': _tmp_entity_type, 'entity': _tmp_entity, 'position': _tmp_pos,
-                             'probability': _tmp_prob})
-            _tmp_entity = []
-            _tmp_prob = []
-            _tmp_entity_type = None
-        return _tmp_entity, _tmp_entity_type, _tmp_prob, _tmp_pos, _out
-
-    probability_sequence = [None] * len(tag_sequence) if probability_sequence is None else probability_sequence
-    assert len(tag_sequence) == len(input_sequence) == len(probability_sequence), str(
-        [len(tag_sequence), len(input_sequence), len(probability_sequence)])
-    out = []
-    tmp_entity = []
-    tmp_prob = []
-    tmp_pos = []
-    tmp_entity_type = None
-    for n, (_l, _i, _prob) in enumerate(zip(tag_sequence, input_sequence, probability_sequence)):
-        if _l.startswith('B-'):
-            _, _, _, _, out = update_collection(tmp_entity, tmp_entity_type, tmp_prob, tmp_pos, out)
-            tmp_entity_type = '-'.join(_l.split('-')[1:])
-            tmp_entity = [_i]
-            tmp_prob = [_prob]
-            tmp_pos = [n]
-        elif _l.startswith('I-'):
-            tmp_tmp_entity_type = '-'.join(_l.split('-')[1:])
-            if len(tmp_entity) == 0:
-                # if 'I' not start with 'B', skip it
-                tmp_entity, tmp_entity_type, tmp_prob, tmp_pos, out = update_collection(
-                    tmp_entity, tmp_entity_type, tmp_prob, tmp_pos, out)
-            elif tmp_tmp_entity_type != tmp_entity_type:
-                # if the type does not match with the B, skip
-                tmp_entity, tmp_entity_type, tmp_prob, tmp_pos, out = update_collection(
-                    tmp_entity, tmp_entity_type, tmp_prob, tmp_pos, out)
-            else:
-                tmp_entity.append(_i)
-                tmp_pos.append(n)
-                tmp_prob.append(_prob)
-        elif _l == 'O':
-            tmp_entity, tmp_entity_type, tmp_prob, tmp_pos, out = update_collection(
-                tmp_entity, tmp_entity_type, tmp_prob, tmp_pos, out)
-        else:
-            raise ValueError('unknown tag: {}'.format(_l))
-    _, _, _, _, out = update_collection(tmp_entity, tmp_entity_type, tmp_prob, tmp_pos, out)
-    return out
-
-
 class NER:
 
     def __init__(self, model_name: str = DEFAULT_MODEL, max_length: int = 128):
         logging.debug(f'loading {model_name}')
         self.config, self.tokenizer, self.model = load_model(model_name, task='token_classification')
         self.max_length = max_length
-        self.id_to_label = {str(v): k for k, v in self.config.label2id.items()}
+        self.id_to_label = {v: k for k, v in self.config.label2id.items()}
         # load crf layer
         self.crf_layer = None
         if 'crf_state_dict' in self.config.to_dict().keys():
@@ -102,6 +51,7 @@ class NER:
                 text: str or List,
                 batch_size: int = None,
                 return_probability: bool = False,
+                return_position: bool = False,
                 skip_preprocess: bool = False):
         single_input_flag = type(text) is str
         text = [text] if single_input_flag else text
@@ -123,28 +73,82 @@ class NER:
                     truncation=True)
                 inputs += encoded_input['input_ids'].cpu().detach().int().tolist()
                 output = self.model(**{k: v.to(self.device) for k, v in encoded_input.items()})
-                prob = torch.softmax(output['logits'], dim=-1)
-                prob, pred = torch.max(prob, dim=-1)
-                prob = prob.cpu().detach().float().tolist()
-                pred = pred.cpu().detach().int().tolist()
+                prob = torch.softmax(output['logits'], dim=-1).cpu().detach().float().tolist()
                 if self.crf_layer is not None:
                     if self.parallel:
-                        best_path = self.crf_layer.module.viterbi_tags(output['logits'])
+                        pred = [tag_seq for tag_seq, _ in self.crf_layer.module.viterbi_tags(output['logits'])]
                     else:
-                        best_path = self.crf_layer.viterbi_tags(output['logits'])
-                    pred = [tag_seq for tag_seq, _ in best_path]
-                probs += prob
-                preds += pred
-        preds = [[self.id_to_label[__p] for __p in _p] for _p in preds]
-        output = {
-            'prediction': preds,
-            'probability': probs,
-            'input': inputs,
-            'entity_prediction': [
-                decode_ner_tags(_p, _i, _prob) for _p, _prob, _i in zip(preds, probs, inputs)
-            ]
-        }
+                        pred = [tag_seq for tag_seq, _ in self.crf_layer.viterbi_tags(output['logits'])]
+                else:
+                    pred = torch.max(prob, dim=-1)[1].cpu().detach().int().tolist()
+                probs += [[prob[n][_n][_p] for _n, _p in enumerate(p)] for n, p in enumerate(pred)]
+                preds += [[self.id_to_label[_p] for _p in p] for p in pred]
+
+        output = [self.decode_ner_tags(p, prob, i, return_probability, return_position)
+                  for p, prob, i in zip(preds, probs, inputs)]
         if single_input_flag:
-            for k in output.keys():
-                output[k] = output[k][0]
+            return output[0]
         return output
+
+    def decode_ner_tags(self,
+                        tag_sequence: List,
+                        probability_sequence: List,
+                        input_sequence: List,
+                        return_probability: bool,
+                        return_position: bool):
+
+        def update_collection(_tmp_entity, _tmp_entity_type, _tmp_prob, _tmp_pos, _out):
+            if len(_tmp_entity) != 0 and _tmp_entity_type is not None:
+                _tmp_data = {'type': _tmp_entity_type, 'entity': self.tokenizer.decode(_tmp_entity)}
+                if return_probability:
+                    _tmp_data['probability'] = mean(_tmp_prob)
+                if return_position:
+                    _tmp_data['position'] = _tmp_pos
+                    _tmp_data['tokenized_input'] = self.tokenizer.convert_ids_to_tokens(input_sequence)
+                _out.append(_tmp_data)
+                _tmp_entity = []
+                _tmp_prob = []
+                _tmp_entity_type = None
+            return _tmp_entity, _tmp_entity_type, _tmp_prob, _tmp_pos, _out
+
+        assert len(tag_sequence) == len(input_sequence) == len(probability_sequence), str(
+            [len(tag_sequence), len(input_sequence), len(probability_sequence)])
+        out = []
+        tmp_entity = []
+        tmp_prob = []
+        tmp_pos = []
+        tmp_entity_type = None
+        for n, (_l, _i, _prob) in enumerate(zip(tag_sequence, input_sequence, probability_sequence)):
+            if _l.startswith('B-'):
+                _, _, _, _, out = update_collection(tmp_entity, tmp_entity_type, tmp_prob, tmp_pos, out)
+                tmp_entity_type = '-'.join(_l.split('-')[1:])
+                tmp_entity = [_i]
+                tmp_prob = [_prob]
+                tmp_pos = [n]
+            elif _l.startswith('I-'):
+                tmp_tmp_entity_type = '-'.join(_l.split('-')[1:])
+                if len(tmp_entity) == 0:
+                    # if 'I' not start with 'B', skip it
+                    tmp_entity, tmp_entity_type, tmp_prob, tmp_pos, out = update_collection(
+                        tmp_entity, tmp_entity_type, tmp_prob, tmp_pos, out)
+                elif tmp_tmp_entity_type != tmp_entity_type:
+                    # if the type does not match with the B, skip
+                    tmp_entity, tmp_entity_type, tmp_prob, tmp_pos, out = update_collection(
+                        tmp_entity, tmp_entity_type, tmp_prob, tmp_pos, out)
+                else:
+                    tmp_entity.append(_i)
+                    tmp_pos.append(n)
+                    tmp_prob.append(_prob)
+            elif _l == 'O':
+                tmp_entity, tmp_entity_type, tmp_prob, tmp_pos, out = update_collection(
+                    tmp_entity, tmp_entity_type, tmp_prob, tmp_pos, out)
+            else:
+                raise ValueError(f'unknown tag: {_l}')
+        _, _, _, _, out = update_collection(tmp_entity, tmp_entity_type, tmp_prob, tmp_pos, out)
+        return out
+
+
+if __name__ == '__main__':
+    _test = ['Jacob Collier is a Grammy-awarded English artist from London.'] * 2
+    _model = NER()
+    _o = _model.predict(_test)
